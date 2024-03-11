f709a642d1cea9f644f9502b613e19bbaa866917
Michael Futch <michael.d.futch@gmail.com>
2024-03-11 15:04:34 -0700

    whitespace

--- test/test_sgg.py
+++ test/test_sgg.py
@@ -89,7 +89,6 @@ def test_teesheet():
     assert teesheets['data']['type'] == 'teesheets'
 
 
-
 def test_seasons():
     username = 'mfutch78@gmail.com'
     password = cloud_utils.access_secret_version("593748364912", "FOREUP_MFUTCH", "latest")
